9fd4daa40dae43c0559b9de6e5059428f4877fbc
JangDongyul <purplebeige12@gmail.com>
2023-05-20 18:32:39 +0900

    5014번 스타트링크 복습

--- Main5014.java
+++ Main5014.java
@@ -1,57 +1,57 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main5014 {
+
     static int F,S,G,U,D;
-    static int[] startLink;
+    static int[] dy;
+    static int[] cnt;
     static boolean[] visit;
 
-    public static void bfs(int start){
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-        while(!q.isEmpty()){
-            int peekStart = q.peek();
+    public static void bfs() {
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.offer(S);
+        visit[S] = true;
+        while(!q.isEmpty()) {
+            int peekY = q.peek();
             q.poll();
-            for(int i=0; i<2; i++){
-                int newStart;
-                if(i==0){
-                   newStart = peekStart+U;
-                }else{
-                    newStart = peekStart-D;
-                }
-                if(1<=newStart && newStart<=F){
-                    if(!visit[newStart]){
-                        visit[newStart] = true;
-                        startLink[newStart] = startLink[peekStart]+1;
-                        q.offer(newStart);
+            for(int i=0; i<2; i++) {
+                int ny = peekY+dy[i];
+                if(1<=ny && ny<=F) {
+                    if(!visit[ny]) {
+                        visit[ny] = true;
+                        cnt[ny]=cnt[peekY]+1;
+                        q.offer(ny);
                     }
                 }
             }
+
         }
+
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         InputStreamReader is = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(is);
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        F= Integer.parseInt(st.nextToken());
-        S= Integer.parseInt(st.nextToken());
-        G= Integer.parseInt(st.nextToken());
-        U= Integer.parseInt(st.nextToken());
-        D= Integer.parseInt(st.nextToken());
-        startLink = new int[F+1];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        F = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        G = Integer.parseInt(st.nextToken());
+        U = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
+        dy = new int[] {U,-D};
+        cnt = new int[F+1];
         visit = new boolean[F+1];
-        visit[S] = true;
-        bfs(S);
-        if((S==G) || (0<startLink[G])) {
-            System.out.println(startLink[G]);
-        }
-        else {
-            System.out.println("use the stairs");
+        bfs();
+        if(cnt[G]!=0) {
+            System.out.println(cnt[G]);
+        }else {
+            if(S==G) {
+                System.out.println(0);
+            }else {
+                System.out.println("use the stairs");
+            }
         }
     }
 }
