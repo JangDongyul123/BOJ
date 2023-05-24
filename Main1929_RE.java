9b26265beea8a35bb4831d3ba0f6de0741d0e9cd
JangDongyul <purplebeige12@gmail.com>
2023-05-24 13:43:39 +0900

    1929번 소수찾기 복습

--- Main1929_RE.java
+++ Main1929_RE.java
@@ -4,35 +4,31 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main1929_RE {
+
+    static int s;
+    static int e;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
-        int M, N;
-        int[]arr;
         InputStreamReader is = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(is);
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        StringBuilder sb =new StringBuilder();
-
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        arr = new int[N+1];
-        for(int i=0; i< arr.length; i++){
+        StringBuilder sb = new StringBuilder();
+        s=Integer.parseInt(st.nextToken());
+        e=Integer.parseInt(st.nextToken());
+        arr= new int[e+1];
+        for(int i=2; i<=e; i++){
             arr[i]=i;
         }
-        arr[1]=0;//소수가 아님
-
-        //에라토스테네스의 체
-        for(int i=2; i< arr.length; i++){
-            if(arr[i]==0)continue;//소수가 아니면 지나간다.
-            for(int j=i+i; j< arr.length; j=j+i){
+        for(int i=2; i<=Math.sqrt(e); i++){
+            if(arr[i]==0) continue;
+            for(int j=i+i; j<=e; j+=i){
                 arr[j]=0;
             }
         }
-
-        for(int i=M; i<= N; i++){
+        for(int i=s; i<=e; i++){
             if(arr[i]!=0){
                 sb.append(arr[i]);
-                sb.append('\n');
-
+                sb.append("\n");
             }
         }
         System.out.println(sb);
