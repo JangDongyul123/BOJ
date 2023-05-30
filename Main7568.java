298fcdccebdfb0028c23d7ca68a6da22a83bac82
JangDongyul <purplebeige12@gmail.com>
2023-05-30 20:32:11 +0900

    7568번

--- Main7568.java
+++ Main7568.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Main7568 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main7568 {
+    public static void main(String[] args) throws IOException {
+
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int y1=0; y1<N ;y1++){
+            int rank=1;
+            for(int y2=0; y2<N; y2++){
+                if(y1==y2){
+                    continue;
+                }
+                if((arr[y2][0]>arr[y1][0]) && (arr[y2][1]>arr[y1][1])){
+                    rank++;
+                }
+            }
+            sb.append(rank);
+            sb.append(" ");
+        }
+        System.out.println(sb);
+    }
 }
