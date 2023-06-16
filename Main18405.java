b143a92bddfc51accfc925920b0337c16a229145
JangDongyul <purplebeige12@gmail.com>
2023-06-16 21:00:45 +0900

    18405번 경쟁적 전염

--- Main18405.java
+++ Main18405.java
@@ -1,94 +1,114 @@
-//미완성
-//빅오 n^3 시간초과 SIB
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main18405 {
-    static int[][] map, map0;
-    static int[][] sec;
+
     static int N,K;
+    static int[][] map;
+    static int S,X,Y;
     static int[] dx = {1,0,-1,0};
     static int[] dy = {0,1,0,-1};
-    public static void bfs(){
-        LinkedList<int []> q = new LinkedList<>();
-        for(int y=0; y<N; y++){
-            for(int x=0; x<N; x++){
-                if(0<map[y][x]){
-                    if(q.isEmpty()){
-                        q.offer(new int[]{y,x});
-                    }else{//비어있지 않다면 비교
-                        int index = q.size();
-                        while(0<=index){
-                            if(map[y][x] <map[q.get( index-1)[0]][q.get( index-1)[1]]  ){
-                                //작은 게 앞으로
-                                index--;//new int[]{y,x}를 넣을 인덱스 감소
-                                if(index==0){
-                                    break;
-                                }
-                            }else{
-                                break;
-                            }
-                        }
-                        if(index<0){
-                            q.add(0, new int[]{y,x});
-                        }
-                        q.add(index, new int[]{y,x});
-                    }
-                }
-            }
+
+    static class Node{
+        int x;
+        int y;
+        int time;
+        int virus;
+        Node(int x, int y, int time, int virus){
+            this.x = x;
+            this.y = y;
+            this.time = time;
+            this.virus = virus;
+        }
+        public int getX(){
+            return x;
         }
 
-        //람다와 effectively final에 대해 공부하자
-        /* q.sort(Comparator.comparing((o1, o2) -> {
-            return (map[o1[0]][o1[1]] -  map[o2[0]][o2[1]]);
-        }));*/
-        while(!q.isEmpty()){
-            int peekY = q.peekFirst()[0];
-            int peekX = q.peekFirst()[1];
-            q.poll();
+        public int getY() {
+            return y;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
+        public int getVirus() {
+            return virus;
+        }
+    }
+
+    static Queue<Node> q = new LinkedList<>();
+    static PriorityQueue<Node> pq = new PriorityQueue<>(
+            (o1,o2)->{
+                return o1.virus - o2.virus;
+            }
+    );
+
+    public static void bfs(){
+        while (!q.isEmpty()){
+            Node peekNode = q.poll();
+            if(peekNode.time==S){
+                return;
+            }
+            int peekY = peekNode.getY();
+            int peekX = peekNode.getX();
             for(int i=0; i<4; i++){
                 int ny = peekY + dy[i];
                 int nx = peekX + dx[i];
-                if((0<=ny) && (ny<N) && (0<=nx) && (nx<N)){
-                    if(0==map[ny][nx]){
-                        map[ny][nx] = map[peekY][peekX];
-                        sec[ny][nx] = sec[peekY][peekX]+1;
-                        q.offer(new int[]{ny,nx});
+                if(0<=ny && 0<= nx && ny<N && nx<N){
+                    if(map[ny][nx] == 0){
+                        map[ny][nx] = peekNode.getVirus();
+                        q.offer(new Node(nx,ny,peekNode.getTime()+1, peekNode.getVirus()));
                     }
                 }
             }
         }
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException {
+
         InputStreamReader is = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(is);
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         map = new int[N][N];
-        map0 = new int[N][N];
-        sec = new int[N][N];
 
         for(int y=0; y<N; y++){
             st = new StringTokenizer(br.readLine()," ");
             for(int x=0; x<N; x++){
-                int num = Integer.parseInt(st.nextToken());
-                map[y][x] =  num;
-                map0[y][x] =  num;
+                map[y][x] = Integer.parseInt(st.nextToken());
+                if(map[y][x]>0){
+                    pq.offer(new Node(x,y,0,map[y][x]));
+                }
             }
         }
-        bfs();
-        st = new StringTokenizer(br.readLine()," ");
-        int S = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
-        int Y = Integer.parseInt(st.nextToken());
-        if(sec[X-1][Y-1]<=S){
-            System.out.println(map[X-1][Y-1]);
-        }else{
-            System.out.println(map0[X-1][Y-1]);
+
+        while(!pq.isEmpty()){
+            q.offer(pq.poll());
         }
+
+        st = new StringTokenizer(br.readLine()," ");
+        S = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+        Y = Integer.parseInt(st.nextToken());
+
+        bfs();
+
+/*
+        for(int y=0; y<N; y++){
+            for(int x=0; x<N; x++){
+                System.out.print(map[y][x]+" ");
+            }
+            System.out.println();
+        }
+*/
+
+        System.out.println(map[X-1][Y-1]);
     }
 }
