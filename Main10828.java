1119dea566937d5c82d2bcc3899798251f67c782
JangDongyul <purplebeige12@gmail.com>
2023-06-22 21:59:51 +0900

    10828번 스택 복습

--- Main10828.java
+++ Main10828.java
@@ -5,45 +5,50 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main10828 {
-    public static void main(String[] args) throws IOException {
+
+    static int N;
+
+    public static void main(String[] args) throws Exception {
+
         InputStreamReader is = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(is);
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
         Stack<Integer> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine()," ");
-            String command = st.nextToken();
-            if("push".equals(command)){
-                int pushNumber = Integer.parseInt(st.nextToken());
-                stack.push(pushNumber);
-            }else if("top".equals(command)){
-                if(stack.isEmpty()){
-                    sb.append(-1);
-                    sb.append('\n');
-                }else{
-                    sb.append(stack.peek());
-                    sb.append('\n');
-                }
-            }else if("size".equals(command)){
-                    sb.append(stack.size());
-                    sb.append('\n');
 
-            }else if("pop".equals(command)){
-                if(stack.isEmpty()){
-                    sb.append(-1);
-                    sb.append('\n');
-                }else{
+        for(int n=0; n<N; n++){
+            st = new StringTokenizer(br.readLine()," ");
+            String s1 = st.nextToken();
+            if(s1.equals("push")){
+                int number = Integer.parseInt(st.nextToken());
+                stack.push(number);
+            }else if(s1.equals("pop")){
+                if(!stack.isEmpty()){
                     sb.append(stack.pop());
-                    sb.append('\n');
+                    sb.append("\n");
+                }else{
+                    sb.append(-1);
+                    sb.append("\n");
                 }
-            }else if("empty".equals(command)){
+            }else if(s1.equals("size")){
+                sb.append(stack.size());
+                sb.append("\n");
+            }else if(s1.equals("empty")){
                 if(stack.isEmpty()){
                     sb.append(1);
-                    sb.append('\n');
+                    sb.append("\n");
                 }else{
                     sb.append(0);
-                    sb.append('\n');
+                    sb.append("\n");
+                }
+            }else if(s1.equals("top")){
+                if(stack.isEmpty()){
+                    sb.append(-1);
+                    sb.append("\n");
+                }else{
+                    sb.append(stack.peek());
+                    sb.append("\n");
                 }
             }
         }
